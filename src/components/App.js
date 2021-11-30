1d47553c9f05430a9dd440140086084b4eb57d72
JUNGWOO <whwjddn200@naver.com>
2021-11-30 18:09:53 +0900

    edit profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import AppRouter from "components/Router.js";
 import { authService } from "Mybase.js";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, updateProfile } from "firebase/auth";
 
 function App() {
   const [init, setInit] = useState(false);
@@ -14,24 +14,42 @@ function App() {
   //유저정보
   const [userObj, setUserObj] = useState(null);
 
-  useEffect(() => {
-    //setIsLogin(authService.currentUser);
+  const getAuth = () => {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLogin(true);
-        setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: () =>
+            updateProfile(user, { displayName: user.displayName }),
+        });
       } else {
         setIsLogin(false);
       }
       setInit(true);
     });
+  };
+
+  useEffect(() => {
+    //setIsLogin(authService.currentUser);
+    getAuth();
   }, []);
+
+  const refreshUser = () => {
+    getAuth();
+    //setUserObj(authService.currentUser);
+  };
 
   return (
     <>
       {init ? (
         //<AppRouter isLogin={Boolean(userObj)} userObj={userObj} />
-        <AppRouter isLogin={isLogin} userObj={userObj} />
+        <AppRouter
+          refreshUser={refreshUser}
+          isLogin={isLogin}
+          userObj={userObj}
+        />
       ) : (
         <>
           <span> 준비중 </span>
